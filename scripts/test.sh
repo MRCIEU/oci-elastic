371478dd491c24eeaf0fb1e8da20cc02ffc44b43
Ben Elsworth <be15516@bristol.ac.uk>
2020-05-20 13:20:26 +0100

    testing performance

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,16 +1,20 @@
 url='localhost'
+port='9300'
 #url='192.168.0.20'
+#url='140.238.83.192'
 
 #curl -XGET $url':9200/_cat/indices/ukb-b*' | sort -k3
 
 #random_snp=rs$((1 + RANDOM % 1000000))
 #echo $random_snp
 
-index_name='UKB-b'
+index_name='ukb-b'
 
 #get 1000 records for random snps
 echo 'Getting random set of SNPs'
-time curl -s -o 1000.out -XGET $url':9200/'$index_name'/_search?pretty' -H 'Content-Type: application/json' -d '{"size":10000,"query":{"bool":{"filter":[{"term":{"study_id":"1"}}]}}}';
+com="curl -s -o 1000.out -XGET '$url:$port/$index_name/_search?pretty' -H 'Content-Type: application/json' -d '{\"size\":10000,\"query\":{\"bool\":{\"filter\":[{\"term\":{\"gwas_id\":\"5776\"}}]}}}';"
+echo $com
+eval $com
 random_snps=`grep snp_id 1000.out | cut -d ' ' -f13 | sed 's/"//g' | sed 's/,//' | sort --random-sort | head -n10`
 echo $random_snps
 
@@ -19,7 +23,7 @@ for i in $random_snps;
 	do
 	echo $i
 	#time curl -o test.$i.out -XGET $url':9200/ukb-b-00/_search?pretty' -H 'Content-Type: application/json' -d '{"size":1000,"query":{"bool":{"filter":[{"term":{"snp_id":"'$i'"}}]}}}';
-	time curl -s -o out/test1.$i.out -XGET $url':9200/'$index_name'/_search?pretty' -H 'Content-Type: application/json' -d '{"size":100000,"query":{"bool":{"filter":[{"term":{"snp_id":"'$i'"}}]}}}';
+	time curl -s -o out/test1.$i.out -XGET $url':'$port'/'$index_name'/_search?pretty' -H 'Content-Type: application/json' -d '{"size":100000,"query":{"bool":{"filter":[{"term":{"snp_id":"'$i'"}}]}}}';
 
 done
 duration=$SECONDS
@@ -33,7 +37,8 @@ for i in {1..10};
 	snpList=\"`grep snp_id 1000.out | cut -d ' ' -f13 | sed 's/"//g' | sed 's/,//' | sort --random-sort | head -n 10 | paste -s -d, - | sed 's/,/","/g'`\"
 	echo "snpList $snpList"
 	echo 'pval search'
-	curl -s -o out/test2.$i.out -XGET $url':9200/'$index_name'/_search?pretty' -H 'Content-Type: application/json' -d '{"size":10000,"query":{"bool":{"filter":[{"terms":{"snp_id":['$snpList']}},{"range" : {"p": {"lt": 1e-5}}}]}}}';
+	#curl -s -o out/test2.$i.out -XGET $url':'$port'/'$index_name'/_search?pretty' -H 'Content-Type: application/json' -d '{"size":10000,"query":{"bool":{"filter":[{"terms":{"snp_id":['$snpList']}},{"range" : {"p": {"lt": 1e-5}}}]}}}';
+	curl -s -o out/test2.$i.out -XGET $url':'$port'/'$index_name'/_search?pretty' -H 'Content-Type: application/json' -d '{"size":10000,"query":{"bool":{"filter":[{"terms":{"snp_id":['$snpList']}}]}}}';
 done
 duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
