a478c01e255f66c818e038caafa2da1ebbb32f05
Ben Elsworth <be15516@bristol.ac.uk>
2020-06-12 12:31:43 +0100

    checking counts

--- scripts/check_gwas_counts.py
+++ scripts/check_gwas_counts.py
@@ -2,19 +2,17 @@ from elasticsearch import Elasticsearch
 import config
 
 #elasticsearch connection
-def es_connection():
-    try:
-        es = Elasticsearch(
-            [{'host': config.elastic_host,'port': config.elastic_port}],
-        )
-        if not es.ping():
-            raise ValueError("Connection failed")
-        else:
-            print('Connected')
-            return es
-    except Exception as e: 
-        print('Elasticsearch error:',e)
-        exit()
+try:
+    es = Elasticsearch(
+        [{'host': config.elastic_host,'port': config.elastic_port}],
+    )
+    if not es.ping():
+        raise ValueError("Connection failed")
+    else:
+        print('Connected')
+except Exception as e: 
+    print('Elasticsearch error:',e)
+    exit()
 
 def gwas_count(gwas_id,index_name):
     filterData=[
@@ -28,7 +26,6 @@ def gwas_count(gwas_id,index_name):
             }
         }
     }
-    es = es_connection()
     res=es.search(
         request_timeout=60,
         index=index_name,
@@ -36,6 +33,25 @@ def gwas_count(gwas_id,index_name):
         )
     return res
 
-es_connection()
-res = gwas_count(gwas_id='ENSG00000100985',index_name='eqtl-a')
-print(res)
+def read_logs(log_file):
+    with open(log_file,'r') as f:
+        for line in f:
+            l = line.split(' ')
+            gwas_id = l[0]
+            index_count=l[-1].rstrip()
+            #print(gwas_id,index_count)
+            index_name = "-".join(gwas_id.split('-')[0:2])
+            gwas_name = gwas_id.split('-')[3].replace('.log','')
+            res = gwas_count(gwas_id=gwas_name,index_name=index_name)
+            try:
+                if int(index_count) != int(res['hits']['total']):
+                    print('Error',index_name,gwas_name,index_count,res['hits']['total'])      
+                else:
+                    print('Good',index_name,gwas_name,index_count,res['hits']['total'])    
+            except:
+                  print('Error',index_name,gwas_name,index_count,res['hits']['total'])   
+
+if __name__ == '__main__':
+    read_logs(config.log_file)
+#res = gwas_count(es,gwas_id='ENSG00000100985',index_name='eqtl-a')
+#print(res)
